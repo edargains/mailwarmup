0f18b4dbc2e0b86de0a3dbaf0b59c8128a1b345e
Medrado, Erick <erick.medrado@sepog.fortaleza.ce.gov.br>
2022-04-11 13:52:04 -0300

    Pool de Envio Finalizado.

--- src/Mail/sd_mail.py
+++ src/Mail/sd_mail.py
@@ -8,28 +8,26 @@ from email.utils import formatdate
 
 def send(date):
     _from = os.getenv('SD_FROM')
+    _from = list(_from.split(', '))
     _to = os.getenv('SD_TO')
-    #_to2 = ['erickdargains@hotmail.com', 'erickmedrado@hotmail.com', 'pmf202204@outlook.com']
     _to = list(_to.split(', '))
-    #print(_to)
-    #print(_to2)
-    exit
     _server = os.getenv('SMTP_SERVER')
     _port = os.getenv('SMTP_PORT')
     _pwd = os.getenv('SMTP_PWD')
+    
+    for i in range(len(_from)):
+        msg = MIMEMultipart()
+        msg['From'] = _from[i]
+        msg['To'] = ", ".join(_to)
+        msg['Date'] = formatdate(localtime=True)
+        msg['Subject'] = f'Relatório de Envios WIN {date}'
 
-    msg = MIMEMultipart()
-    msg['From'] = _from
-    msg['To'] = ", ".join(_to)
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = f'Relatório de Envios WIN {date}'
+        body = f'Segue Relatório WIN de Envios {date}. \n\nAt.te, \nInfraestrutura COGECT'
+        msg.attach(MIMEText(body, 'plain'))
 
-    body = f'Segue Relatório WIN de Envios {date}. \n\nAt.te, \nInfraestrutura COGECT'
-    msg.attach(MIMEText(body, 'plain'))
-
-    s = smtplib.SMTP(_server, _port)
-    s.starttls()
-    s.login(_from, _pwd)
-    text = msg.as_string()
-    s.sendmail(_from, _to, text)
-    s.quit()
+        s = smtplib.SMTP(_server, _port)
+        s.starttls()
+        s.login(_from[i], _pwd)
+        text = msg.as_string()
+        s.sendmail(_from[i], _to, text)
+        s.quit()
--- src/sender.py
+++ src/sender.py
@@ -14,13 +14,13 @@ import time
 from Mail import sd_mail
 
 def main():
-    i = 0
+    #i = 0
     while True:
         date_and_time = datetime.now()
         date_and_time = date_and_time.strftime('%d/%m/%Y %H:%M:%S')
         sd_mail.send(date_and_time)
-        i += 1
-        time.sleep(66)
+     #   i += 1
+        time.sleep(120)
 
 if __name__ == "__main__":
         load_dotenv()
