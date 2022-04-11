a5a4011032b6c77ca3be1f640ef9e607201ae2de
Medrado, Erick <erick.medrado@sepog.fortaleza.ce.gov.br>
2022-04-11 16:03:00 -0300

    Tag

--- src/sender.py
+++ src/sender.py
@@ -17,7 +17,7 @@ def main():
         date_and_time = datetime.now()
         date_and_time = date_and_time.strftime('%d/%m/%Y %H:%M:%S')
         sd_mail.send(date_and_time)
-        time.sleep(random.randint(65,120))
+        time.sleep(random.randint(61,120))
 
 if __name__ == "__main__":
         load_dotenv()
